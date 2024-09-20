8f35be64a22e10d20e27ec1796d3ac93200e43cd
quanghia <nghiaqcqs@gmail.com>
2024-09-20 22:55:40 +0700

    remove p tag

--- Frontend/src/pages/Main/index.jsx
+++ Frontend/src/pages/Main/index.jsx
@@ -10,8 +10,8 @@ const Main = () => {
         cardClassName="w-40 bg-transparent border-none"
         contentClassName="font-sans text-base font-medium text-gray-600"
       />
-      <h1>This is the home page</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum fugit, voluptatum iusto mollitia sed accusantium nobis pariatur totam magni porro, quae debitis! Recusandae, nulla tempora nobis asperiores quia nisi temporibus.</p>
+      <div>This is the home page</div>
+      
     </div>
   );
 };
