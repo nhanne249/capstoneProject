50e0da5566ef1989f58bce5a56dbc38fdf09eb18
quanghia <nghiaqcqs@gmail.com>
2024-09-20 22:52:18 +0700

    change color

--- Frontend/src/pages/Main/index.jsx
+++ Frontend/src/pages/Main/index.jsx
@@ -8,8 +8,10 @@ const Main = () => {
         imgLink="https://lib.hcmut.edu.vn/uploads/noidung/giao-trinh-giai-tich-1-0-830.jpg"
         content="Giai tich 1"
         cardClassName="w-40 bg-transparent border-none"
-        contentClassName="font-sans text-base font-medium text-gray-500"
+        contentClassName="font-sans text-base font-medium text-gray-600"
       />
+      <h1>This is the home page</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum fugit, voluptatum iusto mollitia sed accusantium nobis pariatur totam magni porro, quae debitis! Recusandae, nulla tempora nobis asperiores quia nisi temporibus.</p>
     </div>
   );
 };
