6e25aee8b012aa8ac63250bb481161424f4247f9
Tan Phat <tnpht7404@gmail.com>
2024-10-14 16:43:17 +0700

    update book module

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,21 +1,24 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './modules/auth/auth.module';
 import { User } from './modules/auth/user.entity';
+import { Book } from './modules/book/book.entity';
 import { UsersModule } from './modules/user/user.module';
+import { BookModule } from './modules/book/book.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
+  imports: [
+  TypeOrmModule.forRoot({
     type: 'mysql',
-    host: 'mysql-1563abc0-daweb.d.aivencloud.com', // 'localhost',
+    host: 'mysql-1563abc0-daweb.d.aivencloud.com',
     port: 22337,
-    username: 'avnadmin', // 'root',
-    password: 'AVNS_Rb6CTJ22C4e_O8CDMSz', // 'Phat_07042004',
-    database: 'defaultdb', // 'mydb1',
-    entities: [User],
+    username: 'avnadmin',
+    password: 'AVNS_Rb6CTJ22C4e_O8CDMSz',
+    database: 'defaultdb',
+    entities: [User, Book],
     synchronize: true,
-  }), AuthModule, UsersModule],
+  }),
+  AuthModule, UsersModule, BookModule],
   controllers: [],
   providers: [],
 })
--- Backend/src/modules/book/book.service.ts
+++ Backend/src/modules/book/book.service.ts
@@ -1,4 +1,64 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, ILike } from 'typeorm';
+import { Book } from './book.entity';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { title } from 'process';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+    constructor(
+        @InjectRepository(Book)
+        private readonly bookRepository: Repository<Book>,
+    ) { }
+
+    async getBookByTitle(title: string): Promise<Book> {
+        return this.bookRepository.findOne({ where: { title: title } });
+    }
+
+    async getBookById(bookId: number): Promise<Book> {
+        return this.bookRepository.findOne({ where: { id: bookId } });
+    }
+    async getAllBooks(page: number): Promise<Book[]> {
+        const pageSize = 10;
+        const offset = (page - 1) * pageSize;
+
+        const books = await this.bookRepository.find({
+            skip: offset,
+            take: pageSize,
+        });
+
+        return books;
+    }
+
+    async deleteBook(bookId: number) {
+        return await this.bookRepository.delete(bookId);
+    }
+
+    async updateBook(book: Book, updateBookDto: UpdateBookDto): Promise<Book> {
+        const updatedBook = this.bookRepository.merge(book, updateBookDto);
+        return this.bookRepository.save(updatedBook);  // Save the updated book
+    }
+
+    async addBook(createBookDto: CreateBookDto): Promise<Book> {
+        const newBook = this.bookRepository.create(createBookDto);
+        return this.bookRepository.save(newBook);
+    }
+
+    async searchBooks(search: string, page: number) {
+        const pageSize = 10;
+        const offset = (page - 1) * pageSize;
+        const books = await this.bookRepository.find({
+            where: [
+                { title: ILike(`%${search}%`) },
+                { author: ILike(`%${search}%`) },
+                { description: ILike(`%${search}%`) }
+            ],
+            select: ['title', 'quantity', 'author', 'description', 'costPrice', 'sellingPrice'],
+            skip: offset,
+            take: pageSize,
+        });
+        return books;
+    }
+}
