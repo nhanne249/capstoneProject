a68896555c86edc469671fcf7c7f7ecce588e2d2
qt04 <qthaipham2k4@gmail.com>
2024-10-18 14:34:53 +0700

    Complete Cart-item

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -5,6 +5,10 @@ import { User } from './modules/auth/user.entity';
 import { Book } from './modules/book/book.entity';
 import { UsersModule } from './modules/user/user.module';
 import { BookModule } from './modules/book/book.module';
+import { CartItemModule } from './modules/cart-item/cart-item.module';
+import { CartItem } from './modules/cart-item/cart-item.entity';
+import { ReviewModule } from './modules/review/review.module';
+import { Review } from './modules/review/review.entity';
 
 @Module({
   imports: [
@@ -15,10 +19,10 @@ import { BookModule } from './modules/book/book.module';
     username: 'avnadmin',
     password: 'AVNS_Rb6CTJ22C4e_O8CDMSz',
     database: 'defaultdb',
-    entities: [User, Book],
+    entities: [User, Book, CartItem, Review],
     synchronize: true,
   }),
-  AuthModule, UsersModule, BookModule],
+  AuthModule, UsersModule, BookModule, CartItemModule, ReviewModule],
   controllers: [],
   providers: [],
 })
