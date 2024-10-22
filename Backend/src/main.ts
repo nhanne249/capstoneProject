6c7aa4fa177bb9ee84512c9ec5574e01b29f56d1
nhanne249 <vunhan2492001@gmail.com>
2024-10-22 17:42:32 +0700

    add dev dependencies into Backend

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,5 +1,8 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,5 +12,10 @@ async function bootstrap() {
     credentials: true,
   });
   await app.listen(3000);
+  
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
