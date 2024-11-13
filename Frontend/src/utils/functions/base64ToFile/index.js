54277fccb1fb3e2ecf0d6ad7628c464658663741
nhanne249 <vunhan2492001@gmail.com>
2024-11-14 00:58:23 +0700

    14/11/2024: Handle Post, Put, Get image

--- Frontend/src/utils/functions/base64ToFile/index.js
+++ Frontend/src/utils/functions/base64ToFile/index.js
@@ -1,18 +1,12 @@
 export default function base64ToFile(base64String) {
     const [prefix, base64Data] = base64String.split(',');
     const mimeType = prefix.match(/:(.*?);/)[1];
-
-    // Chuyển đổi chuỗi base64 thành mảng byte
     const byteCharacters = atob(base64Data);
     const byteNumbers = new Array(byteCharacters.length);
     for (let i = 0; i < byteCharacters.length; i++) {
         byteNumbers[i] = byteCharacters.charCodeAt(i);
     }
     const byteArray = new Uint8Array(byteNumbers);
-
-    // Tạo đối tượng Blob từ mảng byte
     const blob = new Blob([byteArray], { type: mimeType });
-
-    // Tạo URL từ Blob và trả về
     return URL.createObjectURL(blob);
 }
