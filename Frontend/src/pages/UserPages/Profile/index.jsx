051412425a38f9e5bbb1d4bb3926c99ba5e0187f
quanghia24 <nghiaqcqs@gmail.com>
2024-11-29 12:08:30 +0700

    Quang Nghia committed - add user profile

--- Frontend/src/pages/UserPages/Profile/index.jsx
+++ Frontend/src/pages/UserPages/Profile/index.jsx
@@ -1,12 +1,93 @@
+import {
+  getUserProfileThunk,
+  updateUserProfileThunk,
+} from "../../../redux/action/user";
+import { getAllOrdersThunk } from "../../../redux/action/order";
+
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { getUserProfileThunk, updateUserProfileThunk } from "../../../redux/action/user";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { Space, Table, Tag } from 'antd';
 import { Button, Input, Form } from "antd";
 
+
 const Profile = () => {
-  const [formEdit] = Form.useForm();
+  const handleShowDetail = (id) => {
+    console.log(id);
+    navigate(`orders/${id}`);
+  };
+
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
+      // render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Date",
+      dataIndex: "date",
+      key: "date",
+    },
+    {
+      title: "Total",
+      dataIndex: "total",
+      key: "total",
+    },
+    {
+      title: "Payment method",
+      dataIndex: "method",
+      key: "method",
+    },
+    {
+      title: "Paid",
+      key: "tags",
+      dataIndex: "tags",
+      render: (_, { tags }) => (
+        <>
+          {tags.map((tag) => {
+            let color = tag === false ? "volcano" : "green";
+            // let color = tag.length > 5 ? 'geekblue' : 'green';
+            // if (tag === 'loser') {
+            //   color = 'volcano';
+            // }
+            return (
+              <Tag color={color} key={tag}>
+                {/* {tag.toUpperCase()} */}
+                isPaid
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: "Action",
+      dataIndex: null,
+      render: (values) => (
+        <Space size="middle">
+          <Tag color={"geekblue"}>
+            <div
+              className="cursor-pointer"
+              onClick={() => {
+                handleShowDetail(values.id);
+              }}
+            >
+              Details
+            </div>
+          </Tag>
+        </Space>
+      ),
+    },
+  ];
+
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [orders, setOrders] = useState();
+  const [isOrderReceive, setIsOrderReceive] = useState(false);
+  const [page, setPage] = useState(1);
+  const [formEdit] = Form.useForm();
   const [profileData, setProfileData] = useState();
   const [isReceived, setIsReceived] = useState(false);
   const [isEditBtnClicked, setIsEditBtnClicked] = useState(false);
@@ -17,6 +98,40 @@ const Profile = () => {
       setIsReceived(true);
     });
   }, [isReceived]);
+
+  // useEffect(() => {
+  //   dispatch(getAllOrdersThunk(page))
+  //   .then((res) => {
+  //     setOrders(res.payload.response);
+  //     setIsOrderReceive(true);
+  //     console.log(res);
+  //   });
+  // }, [isOrderReceive]);
+
+  const data = [
+    {
+      id: '1',
+      method: 'MoMo',
+      total: `$${32}`,
+      date: '2024-10-27',
+      tags: [false],
+    },
+    {
+      id: '2',
+      method: 'MoMo',
+      total: `$${32}`,
+      date: '2024-10-27',
+      tags: [true],
+    },
+    {
+      id: '3',      
+      method: 'MoMo',
+      total: `$${32}`,
+      date: '2024-10-27',
+      tags: [true],
+    },
+  ];
+
 
   const handleEditData = (values) => {
     dispatch(updateUserProfileThunk(values)).then((res) => {
@@ -49,66 +164,161 @@ const Profile = () => {
   };
   return (
     isReceived && (
-      <div className="flex flex-col w-full">
-        <h1 className="text-5xl font-bold h-auto mb-5 mt-4 text-sky-800">Account</h1>
-        <Form
-          layout="horizontal"
-          onFinish={handleEditData}
-          className="w-full"
-          form={formEdit}
-          disabled={!isEditBtnClicked}
-          initialValues={{
-            name: profileData?.name,
-            email: profileData?.email,
-            phone: profileData?.phone,
-            username: profileData?.username,
-          }}
-        >
-          <div className="flex flex-col w-1/2">
-            <div className="px-2 pb-1 font-medium text-2xl">Name</div>
-            <Form.Item name="name">
-              <Input className="!w-[600px] rounded-lg border-gray-600 content-center h-14 text-2xl !text-stone-800" />
-            </Form.Item>
-          </div>
-          <div className="flex flex-row gap-5 justify-between w-fit">
-            <div className="flex flex-col w-1/2">
-              <div className="px-2 pb-1 font-medium text-2xl">Email</div>
-              <Form.Item name="email">
-                <Input className="!w-[600px] rounded-lg border-gray-600 content-center h-14 text-2xl !text-stone-800" />
-              </Form.Item>
+      // <div className="flex flex-col w-full">
+      //   <h1 className="text-5xl font-bold h-auto mb-5 mt-4 text-sky-800">Account</h1>
+      //   <Form
+      //     layout="horizontal"
+      //     onFinish={handleEditData}
+      //     className="w-full"
+      //     form={formEdit}
+      //     disabled={!isEditBtnClicked}
+      //     initialValues={{
+      //       name: profileData?.name,
+      //       email: profileData?.email,
+      //       phone: profileData?.phone,
+      //       username: profileData?.username,
+      //     }}
+      //   >
+      //     <div className="flex flex-col w-1/2">
+      //       <div className="px-2 pb-1 font-medium text-2xl">Name</div>
+      //       <Form.Item name="name">
+      //         <Input className="!w-[600px] rounded-lg border-gray-600 content-center h-14 text-2xl !text-stone-800" />
+      //       </Form.Item>
+      //     </div>
+      //     <div className="flex flex-row gap-5 justify-between w-fit">
+      //       <div className="flex flex-col w-1/2">
+      //         <div className="px-2 pb-1 font-medium text-2xl">Email</div>
+      //         <Form.Item name="email">
+      //           <Input className="!w-[600px] rounded-lg border-gray-600 content-center h-14 text-2xl !text-stone-800" />
+      //         </Form.Item>
+      //       </div>
+      //       <div className="flex flex-col w-1/2">
+      //         <div className="px-2 pb-1 font-medium text-2xl">Phone</div>
+      //         <Form.Item name="phone">
+      //           <Input className="!w-[600px] h-14 rounded-lg border-gray-600 content-center text-2xl !text-stone-800" />
+      //         </Form.Item>
+      //       </div>
+      //     </div>
+      //     <div className="flex flex-col w-1/2">
+      //       <div className="px-2 pb-1 font-medium text-2xl">Username</div>
+      //       <Form.Item name="username">
+      //         <Input className="!w-[600px] h-14 rounded-lg border-gray-600 content-center text-2xl !text-stone-800" />
+      //       </Form.Item>
+      //     </div>
+      //     {isEditBtnClicked && (
+      //       <Form.Item>
+      //         <Button
+      //           className="w-52 h-12 z-10 mb-5 bg-sky-300 !text-white relative font-semibold after:-z-20 after:absolute after:h-1 after:w-1 after:bg-sky-800 after:left-5 overflow-hidden after:bottom-0 after:translate-y-full after:rounded-md after:hover:scale-[300] after:hover:transition-all after:hover:duration-700 after:transition-all after:duration-700 transition-all duration-700 [text-shadow:3px_5px_2px_#075985;] hover:[text-shadow:2px_2px_2px_#7dd4fc] text-2xl"
+      //           htmlType="submit"
+      //         >
+      //           Save
+      //         </Button>
+      //       </Form.Item>
+      //     )}
+      //   </Form>
+      //   {!isEditBtnClicked && (
+      //     <Button
+      //       className="w-52 h-12 z-10 mb-5 bg-sky-300 !text-white relative font-semibold after:-z-20 after:absolute after:h-1 after:w-1 after:bg-sky-800 after:left-5 overflow-hidden after:bottom-0 after:translate-y-full after:rounded-md after:hover:scale-[300] after:hover:transition-all after:hover:duration-700 after:transition-all after:duration-700 transition-all duration-700 [text-shadow:3px_5px_2px_#075985;] hover:[text-shadow:2px_2px_2px_#7dd4fc] text-2xl"
+      //       onClick={() => setIsEditBtnClicked(true)}
+      //     >
+      //       Edit
+      //     </Button>
+      //   )}
+      // </div>
+      <div className="container-feedback">
+        <section>
+          <div className="container mx-auto py-5">
+            <div className="flex flex-wrap">
+              <div className="lg:w-1/3 mr-6 mb-4">
+                {/* User Profile Card */}
+                <div className="bg-white shadow rounded-lg p-4 text-center">
+                  <div className="flex justify-center mb-4">
+                    
+                  </div>
+                  <h5 className="text-xl font-bold mb-2">
+                    {profileData?.name}
+                  </h5>
+                  {/* <p className="text-gray-500 mb-1">
+                    {userData.is_staff
+                      ? "Signed in as an ADMIN"
+                      : "Signed in as a CUSTOMER"}
+                  </p> */}
+                  <p className="text-gray-400 mb-4">{profileData?.email}</p>
+                  <div className="flex justify-center space-x-2">
+                    <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+                      Edit
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <Table className="w-full lg:w-3/5" columns={columns} dataSource={data} />;
+
+
+              <div className="w-full lg:w-1/3">
+                <div className="bg-white shadow rounded-lg mb-4 p-4">
+                  {[
+                    { label: "Full Name", value: profileData?.name },
+                    { label: "Username", value: profileData?.username },
+                    { label: "Email", value: profileData?.email },
+                    {
+                      label: "Phone",
+                      value: profileData?.phone || "(+82) 123 456 789",
+                    },
+                  ].map((item, index) => (
+                    <div
+                      key={index}
+                      className="flex justify-between py-2 border-b"
+                    >
+                      <span className="font-medium text-gray-600">
+                        {item.label}
+                      </span>
+                      <span className="text-gray-800">{item.value}</span>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              {/* 
+                <div className="bg-white shadow rounded-lg">
+                  <ul className="divide-y divide-gray-200">
+                    {orderLog
+                      .sort(
+                        (a, b) =>
+                          new Date(b.timer_start) - new Date(a.timer_start)
+                      )
+                      .slice(0, 4)
+                      .map((log, index) => (
+                        <li key={index} className="p-3">
+                          <div>
+                            <p>
+                              <span className="text-red-500 text-xl font-bold">
+                                {-log.page_cost}
+                              </span>{" "}
+                              {log.status === "pending" ? (
+                                <span className="text-yellow-600 font-bold">
+                                  [ON PENDING]
+                                </span>
+                              ) : (
+                                <span className="text-green-600 font-bold">
+                                  [DONE]
+                                </span>
+                              )}
+                              <span className="text-gray-500 ml-2">
+                                [{new Date(log.timer_start).toLocaleString()}]
+                              </span>
+                            </p>
+                            <p>
+                              <strong>Order's name:</strong> {log.order_name}
+                            </p>
+                          </div>
+                        </li>
+                      ))}
+                  </ul>
+                </div>
+              </div> */}
             </div>
-            <div className="flex flex-col w-1/2">
-              <div className="px-2 pb-1 font-medium text-2xl">Phone</div>
-              <Form.Item name="phone">
-                <Input className="!w-[600px] h-14 rounded-lg border-gray-600 content-center text-2xl !text-stone-800" />
-              </Form.Item>
-            </div>
           </div>
-          <div className="flex flex-col w-1/2">
-            <div className="px-2 pb-1 font-medium text-2xl">Username</div>
-            <Form.Item name="username">
-              <Input className="!w-[600px] h-14 rounded-lg border-gray-600 content-center text-2xl !text-stone-800" />
-            </Form.Item>
-          </div>
-          {isEditBtnClicked && (
-            <Form.Item>
-              <Button
-                className="w-52 h-12 z-10 mb-5 bg-sky-300 !text-white relative font-semibold after:-z-20 after:absolute after:h-1 after:w-1 after:bg-sky-800 after:left-5 overflow-hidden after:bottom-0 after:translate-y-full after:rounded-md after:hover:scale-[300] after:hover:transition-all after:hover:duration-700 after:transition-all after:duration-700 transition-all duration-700 [text-shadow:3px_5px_2px_#075985;] hover:[text-shadow:2px_2px_2px_#7dd4fc] text-2xl"
-                htmlType="submit"
-              >
-                Save
-              </Button>
-            </Form.Item>
-          )}
-        </Form>
-        {!isEditBtnClicked && (
-          <Button
-            className="w-52 h-12 z-10 mb-5 bg-sky-300 !text-white relative font-semibold after:-z-20 after:absolute after:h-1 after:w-1 after:bg-sky-800 after:left-5 overflow-hidden after:bottom-0 after:translate-y-full after:rounded-md after:hover:scale-[300] after:hover:transition-all after:hover:duration-700 after:transition-all after:duration-700 transition-all duration-700 [text-shadow:3px_5px_2px_#075985;] hover:[text-shadow:2px_2px_2px_#7dd4fc] text-2xl"
-            onClick={() => setIsEditBtnClicked(true)}
-          >
-            Edit
-          </Button>
-        )}
+        </section>
       </div>
     )
   );
