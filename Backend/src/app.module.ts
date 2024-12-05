66fd840824cbadd6dfd8863206551e00708c98fb
Nguyễn Ngô Vũ Nhân <vunhan2492001@gmail.com>
2024-12-05 17:00:07 +0700

    hide DB infor to .env

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -6,7 +6,7 @@ import { User } from './modules/auth/user.entity';
 import { Book } from './modules/book/entity/book.entity';
 import { UsersModule } from './modules/user/user.module';
 import { BookModule } from './modules/book/book.module';
-import { env } from 'process';
+import { ConfigModule,ConfigService } from '@nestjs/config';
 import { CartItemModule } from './modules/cart-item/cart-item.module';
 import { CartItem } from './modules/cart-item/cart-item.entity';
 import { ReviewModule } from './modules/review/review.module';
@@ -17,16 +17,23 @@ import { Image } from './modules/book/entity/image.entity';
 
 @Module({
   imports: [
-  TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'mysql-1563abc0-daweb.d.aivencloud.com',
-    port: 22337,
-    username: 'avnadmin',
-    password: 'AVNS_Rb6CTJ22C4e_O8CDMSz',
-    database: 'defaultdb',
-    entities: [User, Book, CartItem, Review, OrderDetail, Image],
-    synchronize: true,
-  }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [User, Book, CartItem, Review, OrderDetail, Image],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
   AuthModule, UsersModule, BookModule, CartItemModule, ReviewModule, OrderDetailModule],
   controllers: [],
   providers: [],
--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 declare const module: any;
 
@@ -11,6 +12,14 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(3000);
   
   if (module.hot) {
