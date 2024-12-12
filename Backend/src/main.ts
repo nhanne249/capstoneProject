d2001c30c5c408fd7d8b8ab87f327a2ce175683a
Nguyễn Ngô Vũ Nhân <vunhan2492001@gmail.com>
2024-12-12 20:21:00 +0700

    create build folder

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
     .addTag('API')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, documentFactory);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(3000);
   
   if (module.hot) {
--- Backend/vercel-func.js
+++ Backend/vercel-func.js
@@ -1,15 +1,17 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory, HttpAdapterHost } from '@nestjs/core';  
 import { ValidationPipe } from '@nestjs/common';  
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';  
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';    
 
 import { AppModule } from './dist/app.module';  
 
 export default async function handler(req, res) { 
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   if (!app) {  
     app.enableCors({  
-      origin: process.env.FRONTEND_DOMAIN || true,  
+      origin: configService.get<string>('FRONTEND_DOMAIN'),  
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,  
     });  
@@ -30,17 +32,20 @@ export default async function handler(req, res) {
       }),  
     );  
 
-    SwaggerModule.setup('api-docs', app, documentFactory,{customJs: [
-'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
-'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
-],
-customCssUrl: [
-'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
-'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
-],});
-
-    // This is important  
+    SwaggerModule.setup('api', app, documentFactory,
+      {
+        customJs: [
+          'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+          'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+        ],
+        swaggerUiEnabled: true,
+        customCssUrl: [
+          'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+          'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+          'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+        ],
+      }
+    );
     await app.init();  
   }  
   const adapterHost = app.get(HttpAdapterHost);  
