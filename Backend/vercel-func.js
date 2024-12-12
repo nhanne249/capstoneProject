990389045d128b47ab8f9150acca594fd8e4bdb3
Nguyễn Ngô Vũ Nhân <vunhan2492001@gmail.com>
2024-12-12 20:49:58 +0700

    create build folder

--- Backend/vercel-func.js
+++ Backend/vercel-func.js
@@ -1,36 +1,35 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory, HttpAdapterHost } from '@nestjs/core';  
 import { ValidationPipe } from '@nestjs/common';  
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';   
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';  
 
 import { AppModule } from './dist/app.module';  
 
-export default async function handler(req, res) { 
-  const app = await NestFactory.create(AppModule);
-  if (!app) {  
+export default async function handler(req, res) {
+  const app = await NestFactory.create(AppModule); 
+  if (app) {  
     app.enableCors({  
-      origin: process.env.FRONTEND_DOMAIN,  
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      origin: process.env.FRONTEND_DOMAIN || true,  
       credentials: true,  
     });  
 
     const config = new DocumentBuilder()  
-      .setTitle('API Documentation') 
-      .setDescription('API Documentation')
+      .setTitle('API Documentation')
+      .setDescription('API Documentation') 
       .setVersion('1.0')  
-      .addTag('API')
+      .addTag('API') 
       .build();  
-    const documentFactory = () => SwaggerModule.createDocument(app, config);  
+    const document = SwaggerModule.createDocument(app, config);  
 
     app.useGlobalPipes(  
-      new ValidationPipe({ 
+      new ValidationPipe({
         whitelist: true, 
         transform: true, 
         always: true,  
       }),  
     );  
 
-    SwaggerModule.setup('api', app, documentFactory);
+    SwaggerModule.setup('api', app, document); 
     await app.init();  
   }  
   const adapterHost = app.get(HttpAdapterHost);  
